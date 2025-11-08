49b3fd55d776e1e12313eff49000fde9519c82d9
Daniel Angkasa <daniel.angkasa@students.mikroskil.ac.id>
2025-11-08 23:26:38 +0700

    belum ada firebase

--- cashease/lib/screens/screen.dart
+++ cashease/lib/screens/screen.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // lib/screen.dart
 import 'package:flutter/material.dart';
 import 'dart:async';
@@ -7,11 +6,6 @@ import 'package:shared_preferences/shared_preferences.dart'; // <-- New import
 // Import file lokal
 import 'login.dart'; // Asumsi login.dart ada di lib/
 import 'home.dart'; // Asumsi home.dart ada di lib/
-=======
-import 'package:flutter/material.dart';
-import 'dart:async';
-import 'login.dart';
->>>>>>> 539719d8a23c2642640c9ed5b5cd4648d69ed0c1
 
 class Screen extends StatefulWidget {
   const Screen({super.key});
@@ -21,7 +15,6 @@ class Screen extends StatefulWidget {
 }
 
 class _ScreenState extends State<Screen> {
-<<<<<<< HEAD
   Future<void> _checkLoginStatus() async {
     final prefs = await SharedPreferences.getInstance();
     final String? phoneNumber = prefs.getString('loggedInPhone');
@@ -50,16 +43,6 @@ class _ScreenState extends State<Screen> {
   void initState() {
     super.initState();
     _checkLoginStatus();
-=======
-  @override
-  void initState() {
-    super.initState();
-    Timer(const Duration(seconds: 3), () async {
-      await Navigator.of(
-        context,
-      ).pushReplacement(MaterialPageRoute(builder: (_) => Login()));
-    });
->>>>>>> 539719d8a23c2642640c9ed5b5cd4648d69ed0c1
   }
 
   @override
--- cashease/macos/Flutter/GeneratedPluginRegistrant.swift
+++ cashease/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-=======
-import sqflite_darwin
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
->>>>>>> 539719d8a23c2642640c9ed5b5cd4648d69ed0c1
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
